2545f6f9645d2a0d92b6e6251c01e398d70fa853
Harish Pal <harish_pal2@baylor.edu>
2022-03-01 14:24:21 -0600

    Updated

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ class Settings:
          
         #bullet Setting
         self.bullet_speed = 1.5
-        self.bullet_width = 3
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3 #This limits the player to three bullets at a time.
